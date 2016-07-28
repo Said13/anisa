e7c2041e00f0b57297224576f52d3f80a9e07473
said13 <abdullah.atkaev@gmail.com>
2016-07-28 10:07:55 +0300

    some changes

--- cluster/php/send-contact.php
+++ cluster/php/send-contact.php
@@ -2,22 +2,20 @@
 
 	$name = @trim(stripslashes($_POST['name'])); 
 	$email = @trim(stripslashes($_POST['email']));
-	$subject = @trim(stripslashes($_POST['subject']));
-	$website = @trim(stripslashes($_POST['website']));
-	$message = @trim(stripslashes($_POST['message'])); 
+	$message = @trim(stripslashes($_POST['message']));
 
 	$email_from = $email;
 	$email_to = 'abdullah.atkaev@gmail.com';//replace with your email
 
-	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Website: ' . $website . "\n\n" . 'Message: ' . $message;
+	$body = 'Имя: ' . $name . "\n\n" . 'Мобильный номер: ' . $email . "\n\n" . 'Message: ' . $message;
 
-	$success = @mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Website: ' . $website . "\n\n" . 'Message: ' . $message);
+	$success = @mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Message: ' . $message);
 	
 ?>
 
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-	<script>alert("Thank you for contact us. As early as possible  we will contact you.");</script>
-	<meta HTTP-EQUIV="REFRESH" content="0; url=../index.html"> 
+	<script>alert("Спасибо что оставили заявку. Мы свяжемся с Вами в скором времени! Ждите звонка!");</script>
+	<meta HTTP-EQUIV="REFRESH" content="0; url=../index.html" charset="utf-8">
 </head>
